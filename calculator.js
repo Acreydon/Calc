084602c899aaab21c595f2d559c4eb88757046b3
Acreydon <214567493+Acreydon@users.noreply.github.com>
2025-10-20 23:29:20 +0200

    yoo calc

---
+++ calculator.js
@@ -0,0 +1,37 @@
+let varnum1= 7;
+let varnum2 = 6;
+let operator = "subtract";
+console.log(operate(operator,varnum1,varnum2));
+
+function operate(operator,num1,num2){
+    let result;
+switch(operator){
+    case "add":
+        result = add(num1,num2);
+        break;
+    case "subtract":
+        result = subtract(num1,num2);
+        break;
+    case "multiply":
+        result = multiply(num1,num2);
+        break;
+    case "divide":
+        result = divide(num1,num2);
+        break;
+};
+return result;
+
+}
+
+function add(num1,num2){
+    return num1+num2
+}
+function subtract(num1,num2){
+    return num1-num2
+}
+function multiply(num1,num2){
+    return num1*num2
+}
+function divide(num1,num2){
+    return num1/num2
+}
